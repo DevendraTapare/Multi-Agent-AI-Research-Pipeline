3749ce4c40f72a570b00f8dd529e64c71c98c011
Devendra Tapare <88935254+DevendraTapare@users.noreply.github.com>
2026-06-18 17:29:06 +0530

    Replace OPENAI_API_KEY with GROQ_API_KEY

--- tools.py
+++ tools.py
@@ -9,7 +9,7 @@ import os
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
 tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
